7d8ebb6947e169d9ab66b6038039581f9d3ef77c
ElSych <55539754+ElSych@users.noreply.github.com>
2022-08-01 12:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+from flask import Flask
+import rasterio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+elevation_file = 'srtm_N55E160.tif'
 
 
-# Press the green button in the gutter to run the script.
+def get_elevation(lat, lon):
+    coords = ((lat, lon), (lat, lon))
+    with rasterio.open(elevation_file) as src:
+        vals = src.sample(coords)
+        for val in vals:
+            elevation = val[0]
+            return elevation
+
+
+
+@app.route('/')
+def mainpage():
+    return 'Welcome to the main page'
+
+
+@app.route('/main/POINT(<float:crd1> <float:crd2>)')
+def point(crd1, crd2):
+    return ('For POINT(' + str(crd1) + ' ' + str(crd2) + ' ' + ')' + ' Elevation is ' + str(get_elevation(crd1, crd2)))
+
+
+@app.route('/main/LINESTRING(<coords>)')
+def linestring(coords):
+    coords = coords.replace(',', '')
+    coordsspl = coords.split()
+    lengh = len(coordsspl)
+    i = 0
+    answer = ''
+    while i < (lengh - 1):
+        answer = answer + str(coordsspl[i]) + ' ' + str(coordsspl[i + 1]) + ' ' + str(get_elevation(float(coordsspl[i]), float(coordsspl[i + 1]))) + ', '
+        i += 2
+        answer = answer[:-2]
+    return 'LINESTRING(' + str(answer) + ')'
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
