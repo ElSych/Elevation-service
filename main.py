65e3b3d0b90aa4f2c5e443fb8818ea7a9da9cbba
ElSych <55539754+ElSych@users.noreply.github.com>
2022-08-02 14:56:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,23 +21,21 @@ def mainpage():
     return 'Welcome to the main page'
 
 
-@app.route('/main/POINT(<float:crd1> <float:crd2>)')
-def point(crd1, crd2):
-    return ('For POINT(' + str(crd1) + ' ' + str(crd2) + ' ' + ')' + ' Elevation is ' + str(get_elevation(crd1, crd2)))
-
-
-@app.route('/main/LINESTRING(<coords>)')
-def linestring(coords):
+@app.route('/main/<somegeometry>(<coords>)')
+def GEOMETRY(somegeometry, coords):
     coords = coords.replace(',', '')
     coordsspl = coords.split()
     lengh = len(coordsspl)
-    i = 0
-    answer = ''
-    while i < (lengh - 1):
-        answer = answer + str(coordsspl[i]) + ' ' + str(coordsspl[i + 1]) + ' ' + str(get_elevation(float(coordsspl[i]), float(coordsspl[i + 1]))) + ', '
-        i += 2
-        answer = answer[:-2]
-    return 'LINESTRING(' + str(answer) + ')'
+    if (somegeometry.upper() == 'POINT'):
+        return ('Thats a POINT(' + str(coordsspl[0]) + ' ' + str(coordsspl[1]) + ' ' + ')' + ' elevation is ' + str(get_elevation(float(coordsspl[0]), float(coordsspl[1]))))
+    elif (somegeometry.upper() == 'LINESTRING'):
+        i = 0
+        answer = ''
+        while i < (lengh - 1):
+            answer = answer + str(coordsspl[i]) + ' ' + str(coordsspl[i + 1]) + ' ' + str(get_elevation(float(coordsspl[i]), float(coordsspl[i + 1]))) + ',' + ' '
+            i += 2
+            answer = answer[:-2]
+        return 'Thats a LINESTRING(' + str(answer) + ')'
 
 if __name__ == '__main__':
     app.run()
